810026334ba9d2ae34642ecd5715d1429852318a
Francisco Anjos <fcariaanjos@gmail.com>
2023-04-21 10:14:08 +0100

    Created class attributes and its constructor

--- GameSix/Foe.cs
+++ GameSix/Foe.cs
@@ -3,4 +3,17 @@
 namespace GameSix
 {
     public class Foe
+    {
+        // Class attributes
+        private string name;
+        private float health;
+        private float shield;
+        //Constructor
+        public Foe(string name)
+        {
+            this.name = name;
+            health = 100;
+            shield = 0;
+        }
+    }
 }
